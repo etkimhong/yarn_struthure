1a4839aa89d2546ad8b06c6718de9957a7862a43
hong <etkimhong@gmail.com>
2024-09-13 12:37:30 +0700

    deply

--- scripts/setup-environment.sh
+++ scripts/setup-environment.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
 # Ensure NVM is installed
 if ! type nvm > /dev/null 2>&1; then
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 fi
-
 # Load NVM
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 

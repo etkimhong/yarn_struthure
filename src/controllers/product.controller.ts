beedef601e736173ec473f52ba8478944662b3c5
hong <etkimhong@gmail.com>
2024-09-16 10:16:59 +0700

    update env

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,34 +1,58 @@
 // src/controllers/ItemController.ts
-import { Controller, Route, Body, Post, Path, Get, Put, Response, Delete, Middlewares, Queries } from 'tsoa';
-import { ProductCreateRequest, ProductGetAllRequest, ProductUpdateRequest } from '../controllers/types/product-request.type';
-import ProductService from '../services/product.service';
-import validateRequest from '../middlewares/validate-input';
-import { ProductPaginatedResponse, ProductResponse } from '../controllers/types/product-response.type';
-import { productCreateSchema } from './schema/productCreateSchema';
+import {
+  Controller,
+  Route,
+  Body,
+  Post,
+  Path,
+  Get,
+  Put,
+  Response,
+  Delete,
+  Middlewares,
+  Queries,
+} from "tsoa";
+import {
+  ProductCreateRequest,
+  ProductGetAllRequest,
+  ProductUpdateRequest,
+} from "../controllers/types/product-request.type";
+import ProductService from "../services/product.service";
+import validateRequest from "../middlewares/validate-input";
+import {
+  ProductPaginatedResponse,
+  ProductResponse,
+} from "../controllers/types/product-response.type";
+import { productCreateSchema } from "./schema/productCreateSchema";
 
-@Route('v1/products')
+@Route("v1/products")
 export class ProductController extends Controller {
-	// Add this: the request might look like: /v1/products?page=1&limit=5&filter={"price":{"min": 2, "max":5}}&sort={"name": "desc"}
+  // Add this: the request might look like: /v1/products?page=1&limit=5&filter={"price":{"min": 2, "max":5}}&sort={"name": "desc"}
   @Get()
-  public async getAllProducts(@Queries() queries: ProductGetAllRequest): Promise<ProductPaginatedResponse> {
+  public async getAllProducts(
+    @Queries() queries: ProductGetAllRequest
+  ): Promise<ProductPaginatedResponse> {
     try {
       const response = await ProductService.getAllProducts(queries);
 
       return {
         message: "success",
-        data: response
-      }
-
+        data: response,
+      };
     } catch (error) {
-      console.error(`ProductsController - getAllProducts() method error: ${error}`)
+      console.error(
+        `ProductsController - getAllProducts() method error: ${error}`
+      );
       throw error;
     }
   }
 
   @Post()
-  @Response(201, 'Created Success')
+  @Response(201, "Created Success")
   @Middlewares(validateRequest(productCreateSchema))
-  public async createItem(@Body() requestBody: ProductCreateRequest): Promise<ProductResponse> {
+  public async createItem(
+    @Body() requestBody: ProductCreateRequest
+  ): Promise<ProductResponse> {
     try {
       const newProduct = await ProductService.createProduct(requestBody);
 
@@ -37,45 +61,50 @@ export class ProductController extends Controller {
         data: {
           name: newProduct.name,
           category: newProduct.category,
-          price: newProduct.price
-        }
-      }
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  @Get('{id}')
-  public async getItemById(@Path() id: string): Promise<ProductResponse> {
-    try {
-      const product = await ProductService.getProductById(id);
-
-      return {
-        message: "success",
-        data: product
+          price: newProduct.price,
+        },
       };
     } catch (error) {
       throw error;
     }
   }
 
-  @Put('{id}')
-  public async updateItem(@Path() id: string, @Body() requestBody: ProductUpdateRequest): Promise<ProductResponse> {
+  @Get("{id}")
+  public async getItemById(@Path() id: string): Promise<ProductResponse> {
     try {
-      const updatedProduct = await ProductService.updateProduct(id, requestBody);
+      const product = await ProductService.getProductById(id);
 
-      return { message: 'success', data: updatedProduct };
+      return {
+        message: "success",
+        data: product,
+      };
     } catch (error) {
       throw error;
     }
   }
 
-  @Delete('{id}')
-  @Response(204, 'Delete Success')
+  @Put("{id}")
+  public async updateItem(
+    @Path() id: string,
+    @Body() requestBody: ProductUpdateRequest
+  ): Promise<ProductResponse> {
+    try {
+      const updatedProduct = await ProductService.updateProduct(
+        id,
+        requestBody
+      );
+
+      return { message: "success", data: updatedProduct };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Delete("{id}")
+  @Response(204, "Delete Success")
   public async deleteItemById(@Path() id: string): Promise<void> {
     try {
       await ProductService.deleteProduct(id);
-
     } catch (error) {
       throw error;
     }
